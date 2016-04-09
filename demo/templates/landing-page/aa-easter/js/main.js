3d7f8604c8e840248de417f041ad780b2cf92f65
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-04-09 20:25:03 +0200

    Demo pages added. Source moved to cloud server. Source folder deleted. Changed download path and updated new entry form with placeholder

---
+++ demo/templates/landing-page/aa-easter/js/main.js
@@ -0,0 +1,23 @@
+
+function aa_action(){
+
+	ga('send', 'event', { eventCategory: 'AA ACTION Essential Option R79 a Month', eventAction: 'Join Now', eventLabel: 'AA ACTION Essential Option R79'});
+}
+
+
+function aa_advantage(){
+
+	ga('send', 'event', { eventCategory: 'AA Advantage Standard R99 a Month', eventAction: 'Join Now', eventLabel: 'AA Advantage Standard R99'});
+}
+
+
+function aa_advantage_plus(){
+
+	ga('send', 'event', { eventCategory: 'AA Advantage Plus R169 Event', eventAction: 'Join Now', eventLabel: 'AA Advantage Plus R169'});
+}
+
+
+function aa_alliance(){
+
+	ga('send', 'event', { eventCategory: 'AA Alliance R69 Event', eventAction: 'Join Now', eventLabel: 'AA Alliance R69 '});
+}
---
+++ demo/templates/landing-page/aa-valentines/js/main.js
@@ -0,0 +1,23 @@
+
+function aa_action(){
+
+	ga('send', 'event', { eventCategory: 'AA Action R79 Event', eventAction: 'Join Now', eventLabel: 'AA Action R79'});
+}
+
+
+function aa_advantage(){
+
+	ga('send', 'event', { eventCategory: 'AA Advantage R99 Event', eventAction: 'Join Now', eventLabel: 'AA Advantage R99 '});
+}
+
+
+function aa_advantage_plus(){
+
+	ga('send', 'event', { eventCategory: 'AA Advantage Plus R169 Event', eventAction: 'Join Now', eventLabel: 'AA Advantage Plus R169'});
+}
+
+
+function aa_alliance(){
+
+	ga('send', 'event', { eventCategory: 'AA Alliance R69 Event', eventAction: 'Join Now', eventLabel: 'AA Alliance R69 '});
+}
