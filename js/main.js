647b16278116060cd533353a34ef535c92a04284
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-04-14 19:06:06 +0200

    Added door to home page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+// Closing Door
+(function($){
+	$(function(){
+	  
+	
+        
+        
+        $('body').click(function(e){
+            
+        e.preventDefault();
+			var menu = $('.menu');
 
+			if (menu.hasClass('in')) {
+				menu.toggleClass('in');
+                $('.navbar').show();
+                $('.home-content').show();
+			}
+			
+
+        });
+        
+        
+
+	}); // end of document ready
+})(jQuery); // end of jQuery name space
