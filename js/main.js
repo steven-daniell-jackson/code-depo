8a03f532ae6ac9bd1c5290e7ce9d54a31f3f9015
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-04-14 20:47:55 +0200

    Fixed issue with anchor tag

--- js/main.js
+++ js/main.js
@@ -1,25 +1,17 @@
 // Closing Door
 (function($){
-	$(function(){
-	  
-	
+	$(function(){  
         
-        
-        $('body').click(function(e){
-            
-        e.preventDefault();
+        $('body').click(function(e){   
+			      
 			var menu = $('.menu');
 
 			if (menu.hasClass('in')) {
 				menu.toggleClass('in');
                 $('.navbar').show();
                 $('.home-content').show();
-			}
-			
-
+			}	
         });
-        
-        
-
+               
 	}); // end of document ready
 })(jQuery); // end of jQuery name space
